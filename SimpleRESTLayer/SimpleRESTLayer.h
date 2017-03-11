d765b44f0f4549c1c7fe77699eaa9c21e054aebc
Graeme Read <graeme@sigma957.net>
2017-03-11 22:41:02 +0000

    Create ‘Cocoa Touch Framework’ project using Xcode 8.2.1

---
+++ SimpleRESTLayer/SimpleRESTLayer.h
@@ -0,0 +1,19 @@
+//
+//  SimpleRESTLayer.h
+//  SimpleRESTLayer
+//
+//  Created by Graeme Read on 11/03/2017.
+//  Copyright © 2017 Graeme Read. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleRESTLayer.
+FOUNDATION_EXPORT double SimpleRESTLayerVersionNumber;
+
+//! Project version string for SimpleRESTLayer.
+FOUNDATION_EXPORT const unsigned char SimpleRESTLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleRESTLayer/PublicHeader.h>
+
+
