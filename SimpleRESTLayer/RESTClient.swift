600c6f4fa040e5146240d2162429fd0ba117540e
Graeme Read <graeme@optimisedlabs.com>
2017-12-13 09:35:49 +0000

    Relocate extension on Data into RESTClient, for consistency with other extensions

--- SimpleRESTLayer/RESTClient.swift
+++ SimpleRESTLayer/RESTClient.swift
@@ -145,3 +145,13 @@ extension HTTPURLResponse {
         return Response(status, headers: allHeaderFields)
     }
 }
+
+extension Data {
+    fileprivate func decode<T: Decodable>(_ type: T.Type, using decoder: JSONDecoder) throws -> T {
+        if let data = self as? T {
+            return data
+        }
+        
+        return try decoder.decode(type, from: self)
+    }
+}
