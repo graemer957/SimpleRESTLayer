5a453a86be8de226dd294549c36eea628ad2645c
Graeme Read <graeme@optimisedlabs.com>
2017-12-30 16:18:36 +0000

    Add map and flapMap to Result

--- SimpleRESTLayer/Result.swift
+++ SimpleRESTLayer/Result.swift
@@ -9,8 +9,20 @@
 import Foundation
 
 public enum Result<Model: Decodable> {
+    /// Successful results have a Response and Model
     case success(Response, Model)
+    
+    /// Failure results contain an Error
     case failure(Error)
+    
+    // Initialise a result from a function that throws
+    public init(_ response: Response, capturing: () throws -> Model) {
+        do {
+            self = .success(response, try capturing())
+        } catch {
+            self = .failure(error)
+        }
+    }
     
     /// Adapter method to convert Result into Response/Model or throw
     public func unwrap() throws -> (Response, Model) {
@@ -19,6 +31,26 @@ public enum Result<Model: Decodable> {
             return (response, model)
         case let .failure(error):
             throw error
+        }
+    }
+    
+    /// Transform result to a new value, potentially of a new type
+    public func map<U>(_ transform: (Model) throws -> U) -> Result<U> {
+        switch self {
+        case let .success(response, model):
+            return Result<U>(response) { try transform(model) }
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+    
+    /// Generate another result of potentially a new type
+    public func flapMap<U>(_ transform: (Response, Model) -> Result<U>) -> Result<U> {
+        switch self {
+        case let .success(response, model):
+            return transform(response, model)
+        case let .failure(error):
+            return .failure(error)
         }
     }
 }
